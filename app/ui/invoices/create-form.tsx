f33f8eb28bd034469b1d17126ebea575fec17e35
Steezy <bermudezalejandro45@gmail.com>
2024-03-25 17:58:55 -0400

    Update create form file - Removed error var

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -132,15 +132,6 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                   </p>
                 ))}
             </div>
-
-            <div id="submit-error" aria-live="polite" aria-atomic="true">
-              {state.errors?.message &&
-                state.errors.message.map((error: string) => (
-                  <p className="mt-2 text-sm text-red-500" key={error}>
-                    {error}
-                  </p>
-                ))}
-            </div>
           </div>
         </fieldset>
       </div>
